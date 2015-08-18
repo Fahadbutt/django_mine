c5797fa3650cd20102dec2cf9d80aeddd72fc850
Fahadbutt <Fahadbutt@users.noreply.github.com>
2015-08-18 09:46:00 +0500

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,9 +17,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+     url(r'^admin/', include(admin.site.urls)),
      url(r'$', include('polls.urls')),
     # url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    
    
 ]
 
